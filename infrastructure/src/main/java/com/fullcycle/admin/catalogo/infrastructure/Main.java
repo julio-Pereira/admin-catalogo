86d5ed19db39f7743a6a73e2d21b2bb80bd6dd42
julio-Pereira <jandrade.pereira1@gmail.com>
2024-11-04 10:16:49 -0300

    feat: add ListCategoriesUseCase

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,10 +1,16 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
-import com.fullcycle.admin.catalogo.application.UseCase;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
-//        System.out.println(new UseCase().execute());
+        System.out.println("Hello, World!");
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
